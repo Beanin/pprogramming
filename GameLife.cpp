ad0f0d63514f0fba411b8a084d63cd8ae647b1ec
Konstantin.Agapov <konstagapov@gmail.com>
2015-09-26 15:02:26 +0300

    modified:   GameLife.cpp

--- GameLife.cpp
+++ GameLife.cpp
@@ -1,18 +1,20 @@
 #include <pthread.h>
 #include <iostream>
 #include <vector>
-#include <semaphore.h>
 #include <random>
 #include <string>
+
 
 using std::vector;
 
 struct TThreadArg {
     vector<vector<int>> *GlobalFieldPtr;
-    vector<vector<int>> *SavedField;
-    pthread_mutex_t SaveMtx;
+    vector<vector<int>> *GlobalStableFieldPtr;
+    pthread_mutex_t *UpdateStableMtx;
+    pthread_cond_t UpdateCV;
+    bool *Printing, *Updating;
     size_t Hight, Width, CornerY, CornerX;
-    pthread_barrier_t *ReadBarrier, *WriteBarrier;
+    pthread_barrier_t *UpdateBarrier, *UpdateStableBarrier;
     bool *Stop, *End;
 };
 
@@ -39,6 +41,19 @@ size_t NeighboursCount(const vector<vector<int>> &Field, size_t x, size_t y) {
     return NeighboursCount;
 }
 
+void SendField(const vector<vector<int>> &Src) {
+    std::cout << "qq" << std::endl;
+}
+
+void PrintStable(const vector<vector<int>> &Src, pthread_mutex_t *UpdateMtx, pthread_cond_t *UpdateCV, const bool &Updating) {
+    pthread_mutex_lock(UpdateMtx);
+    if (Updating) {
+        pthread_cond_wait(UpdateCV, UpdateMtx);
+    }
+    SendField(Src);
+    pthread_mutex_unlock(UpdateMtx);
+}
+
 void* Handle(void* args) {
     TThreadArg ThrdArg = *((TThreadArg*) args);
     delete (TThreadArg*)args;
@@ -47,7 +62,7 @@ void* Handle(void* args) {
     while (!(*end)) {
         IterNumer = 1;
         while (!(*Stop)) {
-            GetUpdate(*GlobalFieldPtr, &Field, CornerY, CornerX);
+            GetUpdate(*GlobalStableFieldPtr, &Field, CornerY, CornerX);
             for (size_t y = 1; y < Field.size() - 1; ++y) {
                 for (size_t x = 1; x < Field[0].size(); ++x) {
                     if (NeighboursCount(Field ,x ,y) == 3 || NeighboursCount(Field, x, y) == 2 && Field[y][x]) 
@@ -56,47 +71,74 @@ void* Handle(void* args) {
                         Field[y][x] = 0;
                 }
             }
-        //CV Read
-            pthread_barrier_wait(ReadBarrier);
             SendRes(Field, GlobalFieldPtr, CornerY, CornerX);
-        //CV Send
-            if (!pthread_barrier_wait() == PTHREAD_BARRIER_SERIAL_THREAD) {
-                pthread_mutex_lock(SaveMtx);
-                *SavedField = *GlobalFieldPtr;
-                pthread_mutex_unlock(SaveMtx);
+            pthread_barrier_wait(UpdateBarrier);
+            pthread_mutex_lock(UpdateStableMtx);
+            Updating = true;
+            pthread_mutex_unlock(UpdateStableMtx);
+            SendRes(Field, GlobalStableFieldPtr, CornerY, CornerX);
+            if (pthread_barrier_wait(UpdateStableBarrier) == PTHREAD_BARRIER_SERIAL_THREAD) {
+                pthread_mutex_lock(UpdateStableMtx);
+                *Updating = false;
+                pthread_cond_signal(UpdateCV, UpdateStableMtx);
+                pthread_mutex_unlock(UpdateStableMtx);
             }
         }
     }
 }
-vector<vector<int>> GetField() {
+
+
+
+
+vector<vector<int>> GetFieldFromFile() {
     vector<vector<int>> Res;
     return Res;
 }
+
+vector<vector<int>> GetRandomField() {
+    size_t 
+}
+
 vector<vector<int>> GenerateField() {
     vector<vector<int>> Res;
     return Res;
 }
 
+void GetData(size_t &ThreadsCount, size_t &Height, size_t &Width, vector<vector<int>> &Field) {
+#ifdef CVS
+    Field = GetFieldFromFile();
+#ifndef
+    Field = GetRandomField();
+#endif
+    Height = Field.size();
+    Width = Field[0].size();
+}
+
 void master() {
     size_t ThreadsCount;
-    vector<vector<int>> PlayFieldEvenIteration = GetField();
-    Height = PlayFieldEvenIteration.size();
-    Width = PlayFieldEvenIteration[0].size();
-    vector<vector<int>> PlayFieldOddIteration(Heigth, vector<int>(Width));
+    vector<vector<int>> FieldTemporary;
+    vector<vector<int>> FieldStable;
+    size_t Height;
+    size_t Width;
+    GetData(ThreadsCount, Height, Width, FieldStable);
     vector<pthread_t> Threads(ThreadsCount);
-    pthread_cond_t UpdateCV;
-    bool Updated;
-    pthread_barrier_t ReadBarrier, WriteBarrier;
-    pthread_barrier_init(&ReadBarrier, nullptr, ThreadsCount);
-    pthread_barrier_init(&WriteBarrier, nullptr, ThreadsCount);
+    pthread_barrier_t UpdateBarrier, UpdateStableBarrierBarrier;
+    pthread_barrier_init(&UpdateBarrier, nullptr, ThreadsCount);
+    pthread_barrier_init(&UpdateStableBarrier, nullptr, ThreadsCount);
+    std::string Request;
+    bool Started = False;
+    while (std::cin >> Request) {
     //Need partition
-    for (size_t ThrdNum = 0; ThrdNum < ThreadsCount; ++ThrdNum) {
-        size_t CellsForThread = Heigth * Width / ThreadsCount;
-        size_t StartingCell = ThrdNum * CellsForThread;
-        pthread_create(&Threads[ThrdNum], NULL, Handle,
-                       (void*)//arg    }
-    for (size_t ThrdNum = 0; ThrdNum < ThreadsCount; ++ThrdNum) {
-        pthread_join(Threads[ThrdNum], NULL);
+        if (Request == "START") {
+          
+            for (size_t ThrdNum = 0; ThrdNum < ThreadsCount; ++ThrdNum) {
+                size_t CellsForThread = Heigth * Width / ThreadsCount;
+                size_t StartingCell = ThrdNum * CellsForThread;
+                pthread_create(&Threads[ThrdNum], NULL, Handle, (void*)//arg    }
+            for (size_t ThrdNum = 0; ThrdNum < ThreadsCount; ++ThrdNum) {
+                pthread_join(Threads[ThrdNum], NULL);
+            }
+        }
     }
 }
 
