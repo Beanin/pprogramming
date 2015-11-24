058242dd0b4036cc6407ac893f66ba3cdebee005
Beanin <konstagapov@ya.ru>
2015-11-25 00:47:37 +0300

    worker done

--- worker.cpp
+++ worker.cpp
@@ -79,138 +79,72 @@ size_t ThreadWorker::NeighboursCount(size_t x, size_t y)
   return Cnt;
 }
 
-void ThreadWorker::LockQueue()
-{
-  
-}
-
 void ThreadWorker::TakeRequests()
 {
-  
+  pthread_mutex_lock(RequestLock);
+  if (!UpdatingQueue)
+    UpdatingQueue = true;
+  for (; RequestQueuePosition < RequestsFromMaster.size(); ++RequestQueuePosition)
+  {
+    Requests.push_back((*RequestsFromMaster)[RequestQueuePosition]);
+  } 
+  Updated++;
+  if (Updated == WorkersCount)
+  {
+    UpdatingQueue = false;
+    Updated = 0;
+  }
+  pthread_mutex_unlock(RequestLock);
 }
 
-/*void TThreadWorker::HandleRequest() 
-{ 
-  if (State == RUNNING) 
+void TThreadWorker::SendCalculations()
+{
+  for (size_t i = 1; i < OldField[0].size() - 1; ++i) 
   {
-    IterNumber++;
-    for (size_t y = 1; y < Field.size() - 1; ++y) 
-    {
-      for (size_t x = 0; x < Field[0].size(); ++x) 
-      {
-        int c = NeighboursCount(x, y);
-        if (OldField[y][x] == 1) {
-          if (c < 2 || c > 3)
-            Field[y][x] = 0;
-
-        }
-        else if (c == 3)
-          Field[y][x] = 1;
-      }
-    }
-    OldField.swap(Field);
-    int waitRes = pthread_barrier_wait(Barrier);
-    if (waitRes != PTHREAD_BARRIER_SERIAL_THREAD && waitRes != 0) 
-    {
-      perror("pthread_barrier_wait");
-    }
-
-  }  
-}*/
-
-void TThreadWorker::SendCalculations() {
-  if (State == RUNNING) 
-  {
-    for (size_t i = 1; i < OldField[0].size() - 1; ++i) 
-    {
-      (*SendT)[i - 1] = OldField[0][i]; 
-      (*SendB)[i - 1] = OldField[Field.size() - 1][i];  
-    }
+    (*SendFieldTop)[i - 1] = OldField[0][i]; 
+    (*SendFieldBottom)[i - 1] = OldField[OldField.size() - 1][i];  
   }
 }
 
 void TThreadWorker::ReceiveCalculations() 
 {
-  if (State == RUNNING) 
-  {
-    for (size_t i = 1; i < Field[0].size() - 1; ++i) 
-    {
-      Field[0][i] = (*recT)[i];
-      Field[Field.size() - 1][i] = (*recB)[i];
-    }
-  }
-}
-
-void TThreadWorker::ReceiveRequest() 
-{
-  pthread_mutex_lock(ReqLock);  
-  *Updating = true;
-  if (id == 0)
-    pthread_cond_signal(ReqCV);
-  pthread_mutex_unlock(ReqLock);
-  if (Request == "STOP") 
-  {
-    State = STOPPED;
-  } 
-  else if (Request == "END") 
-  {
-    if (State == STOPPED)
-      State = ENDED;
-  }  
-  else if (Request == "RUN") 
-  {
-    IterCount = req->IterCount();
-    if (Reported)
-      Reported = false;
-  }
-  int waitRes = pthread_barrier_wait(Barrier);
-  if (waitRes != PTHREAD_BARRIER_SERIAL_THREAD && waitRes != 0) 
-  {
-     perror("pthread_barrier_wait");
-  }
-  else if (waitRes == PTHREAD_BARRIER_SERIAL_THREAD) 
-  {
-    pthread_mutex_lock(ReqLock);
-    pthread_cond_signal(ReqCV, ReqLock);
-    *Updating = false;
-    pthread_mutex_unlock(ReqLock);
-  }     
-}
-
-
-
-
-
-void ThreadWorker::StopWait() 
-{
-  int waitRes = pthread_barrier_wait(Barrier);
-  if (waitRes != PTHREAD_BARRIER_SERIAL_THREAD && waitRes != 0) 
-  {
-    perror("pthread_barrier_wait");
-  }
-  if (!Id) 
-    pthread_cond_signal(StopCV); 
-}
-
-void ThreadWorker::SendCommonReport() 
-{
   for (size_t i = 1; i < OldField[0].size() - 1; ++i) 
   {
-    (*SendField)[0][i-1] = OldField[i]; 
-    (*SendField)[SendField->size() - 1][i-1] = OldField[OldField.size() - 1][i];
-  } 
+    OldField[0][i] = (*ReceiveFieldTop)[i];
+    OldField[OldField.size() - 1][i] = (*ReceiveFieldBottom)[i];
+  }
+}
+
+void TThreadWorker::CollabSync()
+{
+  pthread_barrier_wait(Barrier);
+}
+
+void TThreadWorker::WakeUpMaster()
+{
+  if (Id == 0) 
+  { 
+    pthread_cond_signal(MasterSleepCV);
+  }
 }
 
 void ThreadWorker::SendFinalReport() 
 {
-  for (size_t i = 0; i < SendField->size() - 1; ++i) 
+  for (size_t i = 0; i < SrcField->size() - 1; ++i) 
   {
-    for (size_t j = 0; j < (*SendField)[0].size();++j) 
+    for (size_t j = 0; j < (*SrcField)[0].size();++j) 
     {
-      (*SendField)[i][j] = OldField[i + 1][j + 1];
+      (*SrcField)[i][j] = OldField[i + 1][j + 1];
     }
   }  
 } 
+
+void TThreadWorker::Sleep()
+{
+  pthread_mutex_lock(WorkersSleepLock);
+  pthread_cond_wait(WorkersSleepCV, WorkersSleepLock);
+  pthread_mutex_unlock(WorkersSleepLock);
+}
 
 size_t ThreadWorker::NeighboursCount(size_t x, size_t y) 
 {
@@ -220,10 +154,24 @@ size_t ThreadWorker::NeighboursCount(size_t x, size_t y)
     for (int j = -1; j < 2; ++j) 
     {
       if (!(i==0 && j==0))
-        Cnt+= OldField[i+y][j + x];
+        Cnt+= OldField[i + y][j + x];
     }
   }
   return Cnt;
 } 
 
-
+void TThreadWorker::Calculate()
+{
+  for (size_t y = 1; y < OldField.size() - 1; ++y)
+  {
+    for (size_t x = 1; x < OldField[0].size() - 1; ++x)
+    {
+      if (OldField[y][x] && (NeighboursCount(x, y) == 3 || NeighboursCount(x, y) == 4))
+        Field[y][x] = 1;
+      else if (OldField[y][x]==0 && NeighboursCount(x, y) == 3)
+        Field[y][x] = 1;
+      else Field[y][x] = 0;   
+    }
+  }
+  Field.swap(OldField);
+}
--- worker.h
+++ worker.h
@@ -32,11 +32,11 @@ public:
   virtual void ReceiveCalculations() = 0;
   virtual void CollabSync() = 0;
   virtual void WakeUpMaster() = 0;
-  virtual void SendCommonReport() = 0;
   virtual void SendFinalReport() = 0;
   virtual void Calculate() = 0;
   virtual void Sleep() = 0;
-  
+
+  virtual void Report() {}
   virtual void HandleOtherRequests() {}
   
   virtual ~BaseWorker() = default;
@@ -53,62 +53,39 @@ protected:
 
 
 
-class ThreadWorkerData
+struct ThreadWorkerData
 {
   pthread_barrier_t* WorkersBarrier;
   pthread_mutex_t* WorkersSleepLock;
   pthread_cond_t* WorkersSleepCV;
-  pthread_mutex_t* MasterSleepLock;
   pthread_cond_t* MasterSleepCV;
+  pthread_mutex_t* RequestLock;
   vector<vector<int>>* SrcField;
-  vector<vector<int>>* SendField;
-  vector<vector<int>>* ReceiveField;
+  vector<vector<int>>* SendFieldTop;
+  vector<vector<int>>* SendFieldBottom;
+  vector<vector<int>>* ReceiveFieldTop;
+  vector<vector<int>>* ReceiveFieldBottom;
+  const vector<BaseRequest>* RequestsFromMaster;
 };
 
 class ThreadWorker : public BaseWorker, public ThreadWorkerData 
 {
 public:
-  virtual void LockQueue();
-  virtual void UnlockQueue(); 
-  virtual MasterSync() override;
+  virtual void MasterSync() override;
   virtual void TakeRequests() override;
   virtual void SendCalculations() override;
   virtual void ReceiveCalculations() override;
   virtual void CollabSync() override;
   virtual void WakeUpMaster() override;
-  virtual void SendCommonReport() override;
   virtual void SendFinalReport() override;
   virtual void Calculate() override;
   virtual void Sleep() override;
-  virtual void Report() override;
-  
+
+
   static bool UpdatingQueue;
-  size_t RequestQueuePosition;  
- /* ThreadWorker();
-  ThreadWorker(unsigned int id, vector<vector<int>>& field, vector<int>* sendT, vector<int>* sendB, vector<int>* recT,
-               vector<int>* recB, BaseRequest* request, pthread_barrier_t* barrier, pthread_mutex_t* reqLock, 
-               pthread_cond_t reqCV, pthread_mutex_t* stopLock, pthread_cond_t* stopCV, bool* updating, pthread_t thrid);
-  void Init() override; 
-  void TakeRequest() override;
-  void SendCommonRecord() override;
-  void SendFinalReport() override;
-  void HandleRequest() override; 
-  void StopWait() override;
-  ~ThreadWorker() {
-    pthread_join(Thrid, NULL);    
-  }
+  static unsigned int Updated; 
+  size_t RequestQueuePosition;
+  pthread_t pid;
 protected:
-  pthread_mutex_t* UpdateMtx;
-  pthread_cond_t* UpdateCV;
-  vector<vector<int>> *SendField;
-  bool* Updating;  
-  pthread_barrier_t* Barrier; 
-  pthread_mutex_t* StopLock;
-  pthread_cond_t* StopCV;  
-  size_t NeighboursCount(size_t , size_t); 
-  bool Reported;
-  pthread_cond_t* ReqCV; 
-  pthread_mutex_t* ReqLock;
-  pthread_t Thrid;
-  BaseRequest StoredRequest;*/
+  size_t NeighboursCount(size_t x, size_t y);
 };
